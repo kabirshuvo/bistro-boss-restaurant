fee2240c1dbbad89bc64173826f57d8c8502d968
kabirshuvo <kabirshuvo19@gmail.com>
2023-05-23 00:49:11 +0600

    Navbar updated

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -1,11 +1,85 @@
-
+import { FaBeer } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div>
-            <h3 className="text-success py-4 text-center">Navbar</h3>
+  const navOptions = (
+    <>
+      <li>
+        <a>Item 1</a>
+      </li>
+      <li tabIndex={0}>
+        <a>
+          Parent
+          <svg
+            className="fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+          >
+            <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+          </svg>
+        </a>
+        <ul className="p-2">
+          <li>
+            <a>Submenu 1</a>
+          </li>
+          <li>
+            <a>Submenu 2</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <a>Item 3</a>
+      </li>
+    </>
+  );
+
+  return (
+    <>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navOptions}
+            </ul>
+          </div>
+          <Link to='/' className="btn btn-ghost normal-case text-xl">
+            <span className="px-1 text-purple-400">Bistro</span>{" "}
+            <FaBeer className="text-purple-400"></FaBeer>{" "}
+            <span className="px-1 text-purple-400">Boss</span>{" "}
+          </Link>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            {navOptions}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Get started</a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
