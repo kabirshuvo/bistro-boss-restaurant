087bfb786da11ee1f9cdaa9bf51412a12d86981a
kabirshuvo <kabirshuvo19@gmail.com>
2023-05-23 00:00:06 +0600

    Tailwind and daisyui setup success fully

---
+++ src/layout/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+  return (
+    <>
+      <button className="btn btn-outline">Button</button>
+      <button className="btn btn-outline btn-primary">Button</button>
+      <button className="btn btn-outline btn-secondary">Button</button>
+      <button className="btn btn-outline btn-accent">Button</button>
+
+      <button className="btn btn-outline btn-info">Info</button>
+      <button className="btn btn-outline btn-success">Success</button>
+      <button className="btn btn-outline btn-warning">Warning</button>
+      <button className="btn btn-outline btn-error">Error</button>
+    </>
+  );
+}
+
+export default App;
