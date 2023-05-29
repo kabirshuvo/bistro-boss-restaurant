2efae1672bb56e9f1d14c0d8e1200606f857c18b
kabirshuvo <kabirshuvo19@gmail.com>
2023-05-29 12:10:57 +0600

    All Users page Updated

--- src/pages/DashBoard/AllUsers/AllUsers.jsx
+++ src/pages/DashBoard/AllUsers/AllUsers.jsx
@@ -1,12 +1,74 @@
-import { FaUsers } from "react-icons/fa";
-
+import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
+import { FaAward, FaTrashAlt, FaUsers } from "react-icons/fa";
 
 const AllUsers = () => {
-    return (
-        <div>
-            <h3>All Users <FaUsers/></h3>
+  const { data: users = [], refetch } = useQuery(["users"], async () => {
+    const res = await fetch(`http://localhost:5000/users`);
+    return res.json();
+  });
+
+  // TODO:
+  const handleDelete = () => {};
+
+  return (
+    <>
+      <Helmet>
+        <title>Bistro Boss | All Users</title>
+      </Helmet>
+      <div className="text-slate-700">
+        users Count: {users.length}
+        <h3>
+          All Users <FaUsers />
+        </h3>
+        <div className="overflow-x-auto">
+          <table className="table table-zebra w-full">
+            {/* head */}
+            <thead>
+              <tr>
+                <th># SL:</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user, index) => (
+                <tr key={user._id}>
+                  <th># {index + 1}</th>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  {/* <td></td> */}
+                  <td>
+                    {user.role === "admin" ? (
+                      "admin"
+                    ) : (
+                      <button className="btn btn-ghost btn-xs bg-info ">
+                        <FaAward />
+                        <span className="text-slate-700 ms-2">
+                          <small>Update USER</small>
+                        </span>
+                      </button>
+                    )}
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => handleDelete(row)}
+                      className="btn btn-ghost btn-xs "
+                    >
+                      <FaTrashAlt></FaTrashAlt>{" "}
+                      <span className="text-warning ms-2">delete USER</span>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default AllUsers;
