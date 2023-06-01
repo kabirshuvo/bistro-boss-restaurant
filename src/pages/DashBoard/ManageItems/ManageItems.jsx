f07807e46eede26e1ac665f9446a5a814f6c7da0
kabirshuvo <kabirshuvo19@gmail.com>
2023-06-01 20:40:14 +0600

    Manage Items page updated

--- src/pages/DashBoard/ManageItems/ManageItems.jsx
+++ src/pages/DashBoard/ManageItems/ManageItems.jsx
@@ -1,11 +1,32 @@
+import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
 import SectionTitle from "../../../components/SectionTitle";
 import useMenu from "../../../hooks/useMenu";
 
 const ManageItems = () => {
   const [menu] = useMenu();
+  const handleDelete = row => {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You Want to Delete This?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Item Deleted!',
+            'Your file has been deleted.',
+            'success'
+          )
+        }
+      })
+  }
 
   return (
-    <div className="w-full p-7">
+    <div className="w-full p-7 ">
       <SectionTitle
         heading="Manage All Items"
         subHeading="Hurry Up"
@@ -16,51 +37,41 @@ const ManageItems = () => {
           {/* head */}
           <thead>
             <tr>
-              <th>
-                #
-              </th>
+              <th>#</th>
+              <th>Food</th>
               <th>Name</th>
-              <th>Image</th>
-              <th>Description</th>
+              <th>Category</th>
+              <th>Price</th>
               <th>Update</th>
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
-            {
-                menu.map(item => <tr key={item._id}>
-                    <td>
-                     # 1
-                    </td>
-                    <td>
-                      <div className="flex items-center space-x-3">
-                        <div className="avatar">
-                          <div className="mask mask-squircle w-12 h-12">
-                            <img
-                              src={item.image}
-                              alt={item.name}
-                            />
-                          </div>
-                        </div>
-                        <div>
-                          <div className="font-bold">{item.name}</div>
-                         
-                        </div>
+            {menu.map((item, index) => (
+              <tr key={item._id}>
+                <td>{index + 1}</td>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img src={item.image} alt={item.name} />
                       </div>
-                    </td>
-                    <td>
-                      Zemlak, Daniel and Leannon
-                    </td>
-                    <td>Purple</td>
-                    <td>
-                      <button className="btn btn-ghost btn-xs">details</button>
-                    </td>
-                    <td>
-                      <button className="btn btn-ghost btn-xs">details</button>
-                    </td>
-                  </tr>)
-            }
-            
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <div className="font-bold">{item.name}</div>
+                </td>
+                <td>{item.category}</td>
+                <td className="text-right">${item.price}</td>
+                <td>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </td>
+                <td>
+                <button onClick={()=>handleDelete(row)}  className="btn btn-ghost btn-xs "><FaTrashAlt></FaTrashAlt> <span className="text-warning ms-2">delete item</span></button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
